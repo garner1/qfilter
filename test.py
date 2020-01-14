2b14dcca96ce5610ddfe17b5b1eca01bd004eb89
garner1 <silvano.garnerone@gmail.com>
2020-01-14 10:34:39 +0100

    bug on parsing fastq file fixed

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import sys
 from Bio import SeqIO
 
-fastq = sys.argv[1] # the decompressed input file
+path_in = sys.argv[1] #path/to/in.fastq
 
-for record in SeqIO.parse(fastq, "fastq"):
-    if all([record.letter_annotations["phred_quality"][ind]>=36 for ind in range(8)]):
-        print(record.format("fastq-sanger"))
+for read in SeqIO.parse(path_in, "fastq"):
+    # print(read.seq)
+    if all([read.letter_annotations["phred_quality"][ind]>=36 for ind in range(8,16)]): #only reads with good quality barcodes
+        print(read.format("fastq-sanger"))
+
+
+
+
